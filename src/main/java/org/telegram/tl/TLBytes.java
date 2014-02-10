95b11f2068c789109775103c0744658c0e6d24a4
Stepan Korshakov <me@ex3ndr.com>
2014-02-10 13:28:15 +0400

    Clean data for TLBytes

--- src/main/java/org/telegram/tl/TLBytes.java
+++ src/main/java/org/telegram/tl/TLBytes.java
@@ -1,5 +1,7 @@
 package org.telegram.tl;
 
+import java.util.Arrays;
+
 /**
  * Created by ex3ndr on 10.02.14.
  */
@@ -7,6 +9,12 @@ public class TLBytes {
     private byte[] data;
     private int offset;
     private int len;
+
+    public TLBytes(byte[] data) {
+        this.data = data;
+        this.offset = 0;
+        this.len = data.length;
+    }
 
     public TLBytes(byte[] data, int offset, int len) {
         this.data = data;
@@ -24,5 +32,12 @@ public class TLBytes {
 
     public int getLength() {
         return len;
+    }
+
+    public byte[] cleanData() {
+        if (offset == 0 && len == data.length) {
+            return data;
+        }
+        return Arrays.copyOfRange(data, offset, len);
     }
 }
